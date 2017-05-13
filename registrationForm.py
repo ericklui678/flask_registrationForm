74a18cbf984891fd2f6f9217065bc39f1662cc93
Erick Lui <erickjin.lui@gmail.com>
2017-05-12 19:02:38 -0700

    added validation for birthday format

--- registrationForm.py
+++ registrationForm.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import re
+import datetime
 app = Flask(__name__)
 
 app.secret_key = 'ThisIsSecret'
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 NAME_REGEX = re.compile('^[a-zA-Z]+$')
+PW_REGEX = re.compile('^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]')
 
 @app.route('/')
 def index():
@@ -38,6 +40,8 @@ def process():
         flash('Password is required', 'error')
     elif len(request.form['password']) <= 8:
         flash('Password needs to be more than 8 characters', 'error')
+    elif not PW_REGEX.match(request.form['password']):
+        flash('Password needs at least 1 uppercase and 1 digit', 'error')
     else:
         validCount += 1
 
@@ -47,6 +51,19 @@ def process():
         flash('Passwords do not match', 'error')
     else:
         validCount += 1
+
+    # now = datetime.datetime.now()
+    current = str(datetime.datetime.now().strftime('%Y-%m-%d'))
+    bday = request.form['date']
+    print current, bday
+    if len(request.form['date']) < 1:
+        flash('Birthday is required')
+    elif len(request.form['date']) > 10:
+        print len(request.form['date'])
+        flash('Birthday is invalid')
+
+    # else:
+    #     validCount += 1
 
     if validCount == 5:
         flash('You have submitted your information!', 'show')
